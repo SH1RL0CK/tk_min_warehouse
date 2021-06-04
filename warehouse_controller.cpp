6f52e0b4b2cbb22c315eacab7d7a7394d04451d1
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2021-06-04 14:49:49 +0200

    Strukturtyp Palette erstellt

--- warehouse_controller.cpp
+++ warehouse_controller.cpp
@@ -1,5 +1,11 @@
 #include "warehouse_controller.h"
 
+Palett::Palett(ProductType _productType, QString _productName, unsigned int _numberOfProducts)
+    : productType(_productType)
+    , productName(_productName)
+    , numberOfProducts(_numberOfProducts)
+{}
+
 WarehouseController::WarehouseController()
 {
 
--- warehouse_controller.h
+++ warehouse_controller.h
@@ -1,6 +1,17 @@
 #ifndef WAREHOUSECONTROLLER_H
 #define WAREHOUSECONTROLLER_H
 
+#include <QString>
+
+enum ProductType { IceCream, Pizza };
+
+struct Palett
+{
+    ProductType productType;
+    QString productName;
+    unsigned int numberOfProducts;
+    Palett(ProductType _productType, QString _productName, unsigned int _numberOfProducts);
+};
 
 class WarehouseController
 {
