9135df2933fe58969545adfc52120b06d990c1af
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2021-06-04 15:19:48 +0200

    Grundlegende Struktur für den Lagerhaus-Controller erstellt

--- warehouse_controller.cpp
+++ warehouse_controller.cpp
@@ -6,7 +6,22 @@ Palett::Palett(ProductType _productType, QString _productName, unsigned int _num
     , numberOfProducts(_numberOfProducts)
 {}
 
-WarehouseController::WarehouseController()
-{
+ShelfCompartment::ShelfCompartment(unsigned int _id)
+    : id(_id)
+    , palett(nullptr)
+    , nextCompartment(nullptr)
+{}
 
+WarehouseController::WarehouseController()
+    : firstCompartment(nullptr)
+{}
+
+ShelfCompartment *WarehouseController::getFirstCompartment() const
+{
+    return firstCompartment;
+}
+
+void WarehouseController::setFirstCompartment(ShelfCompartment *_firstCompartment)
+{
+    firstCompartment = _firstCompartment;
 }
--- warehouse_controller.h
+++ warehouse_controller.h
@@ -13,10 +13,37 @@ struct Palett
     Palett(ProductType _productType, QString _productName, unsigned int _numberOfProducts);
 };
 
+struct ShelfCompartment
+{
+    unsigned int id;
+    Palett *palett;
+    ShelfCompartment *nextCompartment;
+    ShelfCompartment(unsigned int _id);
+};
+
 class WarehouseController
 {
+private:
+    ShelfCompartment *firstCompartment;
 public:
     WarehouseController();
+    void setFirstCompartment(ShelfCompartment *_firstCompartment);
+    ShelfCompartment *getFirstCompartment() const;
+    void createWarehouseList();
+    ShelfCompartment *searchForCompartment(unsigned int id);
+    void storePalettInCompartment(
+            unsigned int compartmentId,
+            ProductType productType,
+            QString productName,
+            unsigned int numberOfProducts
+     );
+    void editPalettInCompartment(
+            unsigned int compartmentId,
+            ProductType newProductType,
+            QString newProductName,
+            unsigned int newNumberOfProducts
+     );
+    void removePalettFromCompartment(unsigned int compartmentId);
 };
 
 #endif // WAREHOUSECONTROLLER_H
