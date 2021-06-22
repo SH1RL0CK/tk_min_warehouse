ed37a9a41ff1da21cc33dcec8bc03f5099db9640
SH1RL0CK <50455686+SH1RL0CK@users.noreply.github.com>
2021-06-22 22:52:11 +0200

    Kommentare hinzuggefügt

--- warehouse_controller.h
+++ warehouse_controller.h
@@ -7,6 +7,9 @@
 #include "database_manager.h"
 #include "temperature_sensor.h"
 
+/**
+ * @brief Diese Klasse ist zur Verwaltung der verketteten Liste und der Temperatursensoren dar
+ */
 class WarehouseController
 {
 private:
@@ -15,8 +18,17 @@ private:
     unsigned int numberOfCompartments;
     unsigned int numberOfTemperatureSensors;
     DatabaseManager *dbManager;
+    /**
+     * @brief Das erste Element in der verketteten Liste
+     */
     ShelfCompartment *firstCompartment;
+    /**
+     * @brief Die 10 Temperatursensoren werden in einem Vector verwaltet
+     */
     std::vector<TemperatureSensor*> temperatureSensors;
+    /**
+     * @brief Erstellt die 10 Temperatursensoren
+     */
     void createTemperatureSensors();
 public:
     WarehouseController();
@@ -25,17 +37,38 @@ public:
     unsigned int getNumberOfCompartments() const;
     ShelfCompartment *getFirstCompartment() const;
     std::vector<TemperatureSensor*> getTemperatureSensors() const;
+    /**
+     * @brief Sucht ein Regalfach in der verketteten Liste
+     * @param id  Id nach der gesucht wird
+     * @return das gefundene Element oder ein Nullpointer wenn nichts gefunden wurde
+     */
     ShelfCompartment *searchForCompartment(unsigned int id);
+    /**
+     * @brief Einlagern einer Palette in einem Regalfach
+     * @param compartmentId  Id des Regalfachs
+     * @param productName  Name des Produkts auf der Palette
+     * @param numberOfProducts  Anzahl des Produkts auf der Palette
+     */
     void storePalettInCompartment(
             unsigned int compartmentId,
             QString productName,
             unsigned int numberOfProducts
      );
+    /**
+     * @brief Bearbeiten einer Palette in einem Regalfach
+     * @param compartmentId  Id des Regalfachs
+     * @param newProductName  (neuer) Name des Produkts auf der Palette
+     * @param newNumberOfProducts  (neue) Anzahl des Produkts auf der Palette
+     */
     void editPalettInCompartment(
             unsigned int compartmentId,
             QString newProductName,
             unsigned int newNumberOfProducts
      );
+    /**
+     * @brief Palette aus Regalfach auslagern
+     * @param compartmentId  Id des Regalfachs
+     */
     void removePalettFromCompartment(unsigned int compartmentId);
 };
 
--- whole_warehouse_dialog.h
+++ whole_warehouse_dialog.h
@@ -8,6 +8,9 @@ namespace Ui {
 class WholeWarehouseDialog;
 }
 
+/**
+ * @brief Dialog, der den Inhalt des gesamten Lagers anzeigt
+ */
 class WholeWarehouseDialog : public QDialog
 {
     Q_OBJECT
@@ -19,7 +22,15 @@ public:
 private:
     Ui::WholeWarehouseDialog *ui;
     WarehouseController *warehouseController;
+    /**
+     * @brief Anzeigen des Gesamten Lager in einer Tabelle
+     */
     void displayWholeWarehouse();
+    /**
+     * @brief Zeigt den Inhalt eines einzelnen Regalfachs in einer Tabellenreihe an
+     * @param rowCount  Nummer der Reihe
+     * @param compartment  Das Regalfach
+     */
     void displayCompartment(int rowCount, ShelfCompartment *compartment);
 };
 
